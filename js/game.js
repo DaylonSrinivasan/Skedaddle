f9d5ae2a77836e5547d640add2929aea68a78163
Daylon Srinivasan <daylon.srinivasan@gmail.com>
2016-10-03 17:45:24 -0700

    shaina fixed the S's!

--- js/game.js
+++ js/game.js
@@ -1,17 +1,36 @@
-setInterval(draw, MILLISECONDS_PER_FRAME);
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+//$(document).ready(function() {
+  setInterval(draw, MILLISECONDS_PER_FRAME);
 
+//});
+function draw() {
   ctx.drawImage(background, col*background.width/NUM_COLS, row*background.height/NUM_ROWS,
                 background.width/NUM_COLS, background.height/NUM_ROWS, 0, 0, B_SIZE, B_SIZE);
+  if(!play){
+    return;
+  }
+  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
   col++;
   if(col==NUM_COLS){
     row++;
     col = 0;
   }
   if(row==NUM_ROWS){
-    console.log("done!");
-    row = 0;
-    col = 0;
+    pause();
   }
+}
+
+function start() {
+  play = true;
+}
+
+function pause() {
+  play = false;
+}
+
+function restart() {
+  row = 0;
+  col = 0;
+  play = false;
 }
