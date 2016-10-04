9f47344da88eea9b7b895935424802344be84a2d
Daylon Srinivasan <daylon.srinivasan@gmail.com>
2016-10-03 17:58:18 -0700

    effing bootstrap

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
   <div ng-app="app">
     <img id="background" src="images/background.png" hidden>
-    <canvas id="myCanvas" width="500" height="500">
+    <canvas  class="col-sm-9 col-lg-6" id="myCanvas" width="500" height="500">
       style="border:1px solid #d3d3d3;">
       Your browser does not support the HTML5 canvas tag.
     </canvas>
